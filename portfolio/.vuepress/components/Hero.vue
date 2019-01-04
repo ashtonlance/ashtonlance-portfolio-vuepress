a7855ffa0cfafd81c97b4559472eb923dc7cdebd
Ashton Lance <ashton@simplefocus.com>
2019-01-03 21:06:14 -0500

    Update technologies repeater

--- portfolio/.vuepress/components/Hero.vue
+++ portfolio/.vuepress/components/Hero.vue
@@ -1,17 +1,12 @@
 <template>
-    <div>
-      <h1 v-if="text" class="title" v-html="text" />
-      <h4>I work with:</h4>
-      <ul>
-        <li
-          v-for="(technology, key) in $page.frontmatter.technologies"
-        >
-        {{ technology }}
-        </li>
-      </ul>
-      <p v-html="$page.frontmatter.hero_description" ></p>
-    </div>
-    
+  <div>
+    <h1 v-if="text" class="title" v-html="text"/>
+    <h2>I work with:</h2>
+    <ul>
+      <li v-for="item in $page.frontmatter.technologies">{{ item.technology }}</li>
+    </ul>
+    <p v-html="$page.frontmatter.hero_description"></p>
+  </div>
 </template>
 
 <script>
@@ -22,25 +17,29 @@ export default {
       required: false
     }
   }
-}
+};
 </script>
 
 <style scoped>
+.title {
+  text-align: center;
+  font-size: calc(21px + (48 - 14) * ((100vw - 300px) / (1600 - 300)));
+  font-weight: 300;
+  line-height: 1.25;
+  letter-spacing: 0.1rem;
+  width: 800px;
+  max-width: 100%;
+  margin: 18vw auto 7vw auto;
+}
+
+@media screen and (min-width: 600px) {
   .title {
-    text-align: center;
-    font-size: calc(21px + (48 - 14) * ((100vw - 300px) / (1600 - 300)));
-    font-weight: 300;
-    line-height: 1.25;
-    letter-spacing: 0.1rem;
-    width: 800px;
-    max-width: 100%;
-    margin: 18vw auto 7vw auto;
+    margin: 12vw auto 7vw auto;
   }
+}
 
-  @media screen and (min-width: 600px) {
-    .title {
-      margin: 12vw auto 7vw auto;
-    }
-  }
-
+ul {
+  margin: 2rem auto 2rem auto;
+  max-width: 800px;
+}
 </style>
